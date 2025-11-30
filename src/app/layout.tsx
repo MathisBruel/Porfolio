98f829988ef1f9d1371ff15af7dbadd8d41421e8
MathisBruel <mathis.bruel17700@gmail.com>
2025-11-30 13:34:58 +0100

    Install plausible

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, JetBrains_Mono } from "next/font/google";
+import Script from "next/script";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 import SmoothScroll from "@/components/SmoothScroll";
@@ -31,6 +32,16 @@ export default function RootLayout({
       <body
         className={`${inter.variable} ${jetbrainsMono.variable} antialiased bg-background text-foreground font-sans`}
       >
+        <Script
+          src="https://plausible.deva2.fr/js/pa-it8WH6fE5kJWu8FR1plAr.js"
+          strategy="afterInteractive"
+        />
+        <Script id="plausible-init" strategy="afterInteractive">
+          {`
+            window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
+            plausible.init()
+          `}
+        </Script>
         <SmoothScroll />
         <CustomCursor />
         <Navbar />
